9487ca75982533b69c9a352cb2f9162219cde98a
Walter <walter.salas@me.com>
2020-04-22 21:20:24 -0500

    variables

---
+++ variables.js
@@ -0,0 +1,25 @@
+
+// “El término «variable» se utiliza aun fuera del ámbito matemático para 
+// designar una cantidad susceptible de tomar distintos valores numéricos 
+// dentro de un conjunto de números especificado.​” Según Wikipedia.
+
+
+// Declarar e Inicializar
+var nombre = "Walter"
+
+// Declarar
+var edad;
+
+// Inicializar
+edad = 30;
+
+// "=" este operador se utiliza para asignar un valor
+
+// variable de tipo array
+var elementos = ["Computadora", "Celular"];
+
+//variable de tipo objeto
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
