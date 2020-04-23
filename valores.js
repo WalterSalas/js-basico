cb234820631f18a248645ffa4d64dfdfdbfd2767
Walter <walter.salas@me.com>
2020-04-22 21:08:16 -0500

    data types

---
+++ valores.js
@@ -0,0 +1,23 @@
+// Tipos de Datos
+// Valores primitivos //
+
+// number
+40 
+
+// string
+"Walter" 
+
+//boolean
+true
+false
+
+//valores vacios
+null
+undefined
+
+// Valores tipo objeto y array //
+[1,2,3,4] // Array
+{ nombre: "Walter" } // Objeto
+
+
+//con "typeof" podemos investigar que tipo de dato es el siguiente valor ejemplo typeof true "Boolean"
