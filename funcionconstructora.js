7c92c197291b924d271be8308ab8ac60c8b43712
Walter <walter.salas@me.com>
2020-04-25 12:56:03 -0500

    constructora

---
+++ funcionconstructora.js
@@ -0,0 +1,12 @@
+
+
+function auto(marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+//Funcion constructora
+
+
+var autoNuevo = new auto("Tesla", "Model 3", 2020)
+//como constuimos un objeto con la funcion constructora
