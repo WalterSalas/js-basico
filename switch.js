71cd7584acd185d022f0c0dddd1f7e5be332ccdf
Walter <walter.salas@me.com>
2020-04-25 10:52:47 -0500

    switch

---
+++ switch.js
@@ -0,0 +1,15 @@
+var numero = 1;
+
+switch(numero){
+    case 1:
+        console.log("Soy uno!");
+        break;
+    case 10:
+        console.log("Soy un Diez")
+        break;
+    case 100:
+        console.log("Soy un Cien!!")
+        break;
+    default:
+        console.log("No soy nada");
+}
